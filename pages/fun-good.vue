699b8298fb7ebe590337d576bb20ad3708314d81
yudate <y0303noki@gmail.com>
2022-08-11 11:03:02 +0900

    first

---
+++ pages/fun-good.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello World!',
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <p class="greeting">{{ greeting }}</p>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
